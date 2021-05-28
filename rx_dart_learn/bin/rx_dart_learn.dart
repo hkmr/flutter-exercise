f2955d6726d62e6c1d8b0c2149723fd7a3e04464
Harsh Kumar <hkmr@aimedlabs.com>
2021-05-28 19:24:59 +0530

    Explore rxDart package

---
+++ rx_dart_learn/bin/rx_dart_learn.dart
@@ -0,0 +1,5 @@
+// import 'package:rx_dart_learn/rx_dart_learn.dart' as rx_dart_learn;
+
+// void main(List<String> arguments) {
+// //   print('Hello world: ${rx_dart_learn.calculate()}!');
+// }
---
+++ rx_dart_learn/test/rx_dart_learn_test.dart
@@ -0,0 +1,8 @@
+// import 'package:rx_dart_learn/rx_dart_learn.dart';
+// import 'package:test/test.dart';
+
+// void main() {
+//   test('calculate', () {
+//     expect(fetchData());
+//   });
+// }
